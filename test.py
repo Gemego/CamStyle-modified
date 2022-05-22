f0c0d87634a37b58adf7069fbf415765163b8596
Gemego <ziangzhang@qq.com>
2022-05-22 13:48:24 +0800

    specify a test camID

--- test.py
+++ test.py
@@ -1,11 +1,15 @@
 from data import DatasetDataLoader
 from models.gan_setup import create_model
+from util import visualizer
 
-if __name__ == '__main__':
+SAVE_ROOT = "results/market/bounding_box_train_camstyle"
+
+
+def test_specify(camA, camB, isTrain, name):
     # 前面设置了一些参数还需要看一下，这个和train逐个对比了一下，只要test和train不冲突的都可以不加
-    data_loader = DatasetDataLoader(isTrain=False)  # 设置为不训练
+    data_loader = DatasetDataLoader(isTrain)  # 设置为不训练
     dataset = data_loader.load_data()
-    model = create_model(False, 'market-c1-c2')
+    model = create_model(isTrain, name)
     # 此处的参数需要注意一下，猜测第一个是istrain,name后续再看看应该谁的
     model.setup()
     for i, data in enumerate(dataset):  # inner loop within one epoch
@@ -15,4 +19,8 @@ if __name__ == '__main__':
         img_path = model.get_image_paths()
         if i % 5 == 0:
             print('processing (%04d)-th image... %s' % (i, img_path))
-        # save_images(visuals, img_path, opt.camA, opt.camB, opt.save_root)
+        visualizer.save_images(visuals, img_path, camA, camB, SAVE_ROOT)
+
+
+if __name__ == '__main__':
+    test_specify(1, 2, False, 'market-c1-c2')
